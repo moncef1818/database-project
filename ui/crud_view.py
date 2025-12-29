225932d038ae39314199c9faa5f217756786af30
moussa <moussakhanfri04@gmail.com>
2025-12-29 17:29:27 +0100

    crud view

--- ui/crud_view.py
+++ ui/crud_view.py
@@ -1,6 +1,18 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QGridLayout, QPushButton, QStackedWidget, QLabel
+from PyQt5.QtWidgets import (
+    QGridLayout,
+    QLabel,
+    QPushButton,
+    QStackedWidget,
+    QVBoxLayout,
+    QWidget,
+)
+
 from ui.department_crud_view import DepartmentView
+from ui.instructor_crud_view import InstructorView  # type: ignore
+from ui.room_crud_view import RoomView
 from ui.student_crud_view import StudentView
+
+
 class CrudView(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -18,7 +30,13 @@ class CrudView(QWidget):
 
         self.student_view = StudentView()
         self.stack.addWidget(self.student_view)
-        
+
+        self.room_view = RoomView()
+        self.stack.addWidget(self.room_view)
+
+        self.instructor_view = InstructorView()
+        self.stack.addWidget(self.instructor_view)
+
         self.stack.setCurrentIndex(0)
 
     def setup_menu_page(self):
@@ -28,12 +46,12 @@ class CrudView(QWidget):
             ("Department", 0, 0),
             ("Student", 0, 1),
             ("Instructor", 1, 0),
-            ("Room", 1, 1)
+            ("Room", 1, 1),
         ]
 
-        for name , row , col in tables:
+        for name, row, col in tables:
             btn = QPushButton(name)
-            btn.setFixedSize(150,100)
+            btn.setFixedSize(150, 100)
             btn.clicked.connect(lambda checked, n=name: self.open_crud(n))
             grid.addWidget(btn, row, col)
 
@@ -43,5 +61,7 @@ class CrudView(QWidget):
             self.stack.setCurrentWidget(self.dept_view)
         if table_name == "Student":
             self.stack.setCurrentWidget(self.student_view)
-
-    
+        if table_name == "Room":
+            self.stack.setCurrentWidget(self.room_view)
+        if table_name == "Instructor":
+            self.stack.setCurrentWidget(self.instructor_view)
